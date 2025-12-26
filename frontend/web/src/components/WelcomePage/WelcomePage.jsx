b0f5e86fefa405a1e239f04bc163a130a2f81e86
Mariia Semenchenko <mariia.semenchenko@epitech.eu>
2025-12-26 16:11:17 +0100

    wip(WelcomePage): added a pass to the HowItWorks section

--- frontend/web/src/components/WelcomePage/WelcomePage.jsx
+++ frontend/web/src/components/WelcomePage/WelcomePage.jsx
@@ -1,22 +1,22 @@
-"use client"
+"use client";
 
 import "./welcomepage.css";
 import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
+import HowItWorks from "./HowItWorks.jsx";
 
 export default function WelcomePage() {
-    return (
-        <div className="welcome-page-wrapper">
-            <Navbar/>
-            <div className="welcome-page-content">
-                <div className="welcome-page-section" id="features">
-                    features
-                </div>
-                <div className="welcome-page-section" id="how-it-works">
-                    how it works
-                </div>
-            </div>
-            <Footer/>
+  return (
+    <div className="welcome-page-wrapper">
+      <Navbar />
+
+      <div className="welcome-page-content">
+        <div className="welcome-page-section" id="features">
+          features
         </div>
-    )
+        <HowItWorks />
+      </div>
+      <Footer />
+    </div>
+  );
 }
