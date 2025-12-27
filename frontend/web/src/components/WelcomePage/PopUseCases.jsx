230e00a9132b95d5f8400b06ade91e976cd7c914
Mariia Semenchenko <mariia.semenchenko@epitech.eu>
2025-12-27 15:45:04 +0100

    feat(WelcomePage): added a section that explains Why choose us (#83)

--- frontend/web/src/components/WelcomePage/PopUseCases.jsx
+++ frontend/web/src/components/WelcomePage/PopUseCases.jsx
@@ -2,19 +2,19 @@
 import "./welcomepage.css"
 
 export default function PopUseCases() {
-  return (
-    <div className="welcome-page-section popular-use-cases" id="use-cases">
-        <h2>Popular use cases</h2>
-        <div className="use-cases-list">
-            <ul>
-                <li>Automatically save email attachments to cloud storage</li>
-                <li>Create tasks from customer support tickets</li>
-                <li>Send notifications for important events</li>
-                <li>Post new blog content to all social media channels</li>
-                <li>Sync contacts across multiple platforms</li>
-                <li>Generate reports and send to your team</li>
-            </ul>
-        </div>
-    </div>
-  )
+    return (
+      <div className="welcome-page-section popular-use-cases" id="use-cases">
+          <h2>Popular use cases</h2>
+          <div className="use-cases-list">
+              <ul>
+                  <li>Automatically save email attachments to cloud storage</li>
+                  <li>Create tasks from customer support tickets</li>
+                  <li>Send notifications for important events</li>
+                  <li>Post new blog content to all social media channels</li>
+                  <li>Sync contacts across multiple platforms</li>
+                  <li>Generate reports and send to your team</li>
+              </ul>
+          </div>
+      </div>
+    )
 };
--- frontend/web/src/components/WelcomePage/WelcomePage.jsx
+++ frontend/web/src/components/WelcomePage/WelcomePage.jsx
@@ -5,19 +5,21 @@ import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
 import HowItWorks from "./HowItWorks.jsx";
 import PopUseCases from "./PopUseCases.jsx";
+import WhyUs from "./WhyUs.jsx";
 
 export default function WelcomePage() {
-  return (
-    <div className="welcome-page-wrapper">
-      <Navbar />
-      <div className="welcome-page-content">
-        <div className="welcome-page-section" id="features">
-          features
+    return (
+        <div className="welcome-page-wrapper">
+            <Navbar />
+            <div className="welcome-page-content">
+                <div className="welcome-page-section" id="features">
+                    features
+                </div>
+                <WhyUs />
+                <PopUseCases />
+                <HowItWorks />
+            </div>
+            <Footer />
         </div>
-        <PopUseCases />
-        <HowItWorks />
-      </div>
-      <Footer />
-    </div>
-  );
+    );
 }
