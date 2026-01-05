634712467137a111fe5981b1446887bf69fd26ad
Mariia Semenchenko <mariia.semenchenko@epitech.eu>
2026-01-05 22:59:33 +0100

    fix(compilation): removed unneccessary import of a removed css file

--- frontend/web/src/components/Homepage/ServiceCard.jsx
+++ frontend/web/src/components/Homepage/ServiceCard.jsx
@@ -1,24 +1,23 @@
 import React from "react";
-import "./servicecard.css";
 
 export default function ServiceCard({ title, color = "#ddd", icons = [], onClick, ghost }) {
-  const safeIcons = Array.isArray(icons) ? icons : [];
-  return (
-    <div
-      className={`service-card ${ghost ? "ghost-card" : ""}`}
-      style={{ background: color, cursor: "pointer" }}
-      onClick={onClick}
-    >
-      <div className="service-card-header">
-        <div className="service-icons">
-          {safeIcons.map((ic, i) => (
-            <span key={i} className="service-icon">{ic}</span>
-          ))}
+    const safeIcons = Array.isArray(icons) ? icons : [];
+    return (
+      <div
+        className={`service-card ${ghost ? "ghost-card" : ""}`}
+        style={{ background: color, cursor: "pointer" }}
+        onClick={onClick}
+      >
+          <div className="service-card-header">
+              <div className="service-icons">
+                  {safeIcons.map((ic, i) => (
+                      <span key={i} className="service-icon">{ic}</span>
+                  ))}
+              </div>
+          </div>
+        <div className="service-card-body">
+            <div className="service-title">{title}</div>
         </div>
-      </div>
-      <div className="service-card-body">
-        <div className="service-title">{title}</div>
-      </div>
     </div>
   );
 }
